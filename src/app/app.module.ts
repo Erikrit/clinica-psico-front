d8548a19f9e7c020bb4235c01a1f71b062494b85
Thales Baliero Takáo <engc.thales@gmail.com>
2018-05-19 14:04:39 -0300

    tela inicial de cadastro de estagiario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContentComponent } from './content/content.component';
+import { ListaEstagiarioComponent } from './lista-estagiario/lista-estagiario.component';
+import { CadastroEstagiarioComponent } from './lista-estagiario/cadastro-estagiario/cadastro-estagiario.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ContentComponent,
+    ListaEstagiarioComponent,
+    CadastroEstagiarioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/lista-estagiario/cadastro-estagiario/cadastro-estagiario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-estagiario',
+  templateUrl: './cadastro-estagiario.component.html',
+  styleUrls: ['./cadastro-estagiario.component.css']
+})
+export class CadastroEstagiarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
